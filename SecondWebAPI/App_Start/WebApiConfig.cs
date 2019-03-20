72a993221ae43983e0a9bf330cf923124bd73a51
Mangesh Auti <mangesh.auti@mitel.com>
2019-03-20 18:03:04 +0530

    Add project files.

---
+++ SecondWebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SecondWebAPI
+{
+    public static class WebApiConfig
+    {
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        {
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+                
+            }
+           
+        }
+
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //from browser request response goes in json format
+            //config.Formatters.Add(new CustomJsonFormatter());
+
+            //Json indentation
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            //Global cross domain ajax support install packet Microsoft.AspNet.WebApi.Cors else use [EnableCorsAttribute("*","*","*")] above controller or method
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            
+            //Globally redirect Http request to HTTPS else use [RequireHttps] above controller or method
+            //config.Filters.Add(new RequireHttpsAttribute());
+
+        }
+    }
+}
---
+++ SecondWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace SecondWebAPI.Controllers
+{
+   
+    public class EmployeeController : ApiController
+    {
+        // GET api/Employee
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities=new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        // GET api/Employee/5
+        public HttpResponseMessage Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity=entities.Employees.FirstOrDefault(e => e.Id==id);
+                if(entity==null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee details not found for Id =" + id);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+            }
+        }
+
+        // POST api/Employee
+        public HttpResponseMessage Post([FromBody]Employee emp)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(emp);
+                    entities.SaveChanges();
+                    var message=Request.CreateResponse(HttpStatusCode.Created,emp);
+                    message.Headers.Location = new Uri(Request.RequestUri + emp.Id.ToString());
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        // PUT api/Employee/5
+        public HttpResponseMessage Put(int id, [FromBody]Employee emp)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.Id == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee details not found for update Id =" + id);
+                    }
+                    else
+                    {
+                        entity.FirstName = emp.FirstName;
+                        entity.LastName = emp.LastName;
+                        entity.Gender = emp.Gender;
+                        entity.Salary = emp.Salary;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        // DELETE api/Employee/5
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.Id == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee details not found for delete Id =" + id);
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
